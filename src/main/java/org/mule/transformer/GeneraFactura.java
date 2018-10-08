53a827c24e11d2e1291bd84ccb830e39249d4112
Amir HTH <uo240753@uniovi.es>
2018-10-08 17:06:13 +0200

    version final 2 ampliaciones

--- src/main/java/org/mule/transformer/GeneraFactura.java
+++ src/main/java/org/mule/transformer/GeneraFactura.java
@@ -26,29 +26,30 @@ public class GeneraFactura  extends AbstractTransformer {
 	@Override
 	protected Object doTransform(Object src, String enc) throws TransformerException {
 		
+		@SuppressWarnings("unchecked")
 		Collection<PedidoSimple> listadops = (Collection<PedidoSimple>) src;
 		String factura="";
-		for (PedidoSimple ps : listadops) {
-		
-		
-		System.out.println("GENERANDO FACTURA...");
-		//PedidoSimple ps =  (PedidoSimple) src;
 		Random r = new Random();
 		int min = 1000000;
 		int max = 10000000;
 		int preciomin  = 10;
 		int preciomax = 150;
-
-
 		
+		System.out.println("GENERANDO FACTURA...");
+		PedidoSimple primerpedido = (PedidoSimple) listadops.toArray()[0];
 		factura += "\n Factura Num.: "+ r.nextInt(max-min) + max; 
 		factura += "\n Empresa: Amazin Books (3463464R) Registro madrid 42,51,1";		
-		factura += "\n Para: "+ps.getNombreCompleto() +" ("+ps.getNIF()+")";		
+		factura += "\n Para: "+primerpedido.getNombreCompleto() +" ("+primerpedido.getNIF()+")";		
 		factura += "\n PEDIDO: ";
-		factura += "\n\t ISBN: "+ps.getISBN() + "\t Cantidad: "+ps.getCantidad();
-		factura += "\n Total: "+ r.nextInt(preciomax-preciomin) + preciomax;
-		System.out.println("FACTURA: "+factura);
+		
+		for (PedidoSimple ps : listadops) {
+
+			factura += "\n\t ISBN: "+ps.getISBN() + "\t Cantidad: "+ps.getCantidad();
+
 		}
+		
+		factura += "\n Total: "+ r.nextInt(preciomax-preciomin) + preciomax;
+		//System.out.println("FACTURA: "+factura);
 		return factura;
 	}
 
